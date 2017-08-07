1d19accdbb84ff92eb1aca5c5002c23d3581f60c
Vidyadhar Pendurkar <vidyadhar.pendurkar@icastx.com>
2017-08-07 15:51:23 +0530

    Event Emitter - Part 2

--- app.js
+++ app.js
@@ -1,9 +1,24 @@
 var events = require('events');
+var util = require('util');
 
-var myEmitter = new events.EventEmitter();
+var Person = function(name) {
+  this.name = name;
+}
 
-myEmitter.on('someEvent', function(msg) {
-  console.log(msg);
+util.inherits(Person, events.EventEmitter);
+
+var vidyadhar = new Person('vidyadhar');
+var datta = new Person('datta');
+var abhay = new Person('abhay');
+
+var people = [vidyadhar, datta, abhay];
+
+people.forEach(function(person) {
+  person.on('speak', function(msg) {
+    console.log(person.name + ' said: ' + msg);
+  })
 })
 
-myEmitter.emit('someEvent', 'The event has emitted');
+vidyadhar.emit('speak', 'I am Web Developer');
+datta.emit('speak', 'I am tech lead');
+abhay.emit('speak', 'I am Data architect');
