0fa45aec7c46c172c4789b66ab7116e73c1912a1
Vidyadhar Pendurkar <vidyadhar.pendurkar@icastx.com>
2017-08-09 17:30:24 +0530

    Middleware and static files

---
+++ Middleware and static files/app.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const ejs = require('ejs');
+var app = express();
+
+app.set('view engine', 'ejs');
+app.use('/styles', express.static('styles'));
+app.get('/', function(req, res){
+  res.render('index');
+});
+
+app.get('/contact', function(req, res){
+  res.render('contact');
+});
+
+app.get('/profile/:name', function(req, res){
+  var data = {age: 27, job: 'Web Developer', hobbies: ['Eating', 'Gaming', 'Music']};
+  res.render('profile', {name: req.params.name, data: data});
+});
+
+app.listen(3000);
