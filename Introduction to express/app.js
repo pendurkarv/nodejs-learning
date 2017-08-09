8711770a5f98e7b65a1bd702e68b55eb63a6ed8e
Vidyadhar Pendurkar <vidyadhar.pendurkar@icastx.com>
2017-08-09 16:57:41 +0530

    Template engines

--- Introduction to express/app.js
+++ Introduction to express/app.js
@@ -1,17 +1,20 @@
-var express = require('express');
-
+const express = require('express');
+const ejs = require('ejs');
 var app = express();
 
+app.set('view engine', 'ejs');
+
 app.get('/', function(req, res){
-  res.send('This is home page.');
+  res.sendFile(__dirname + '/index.html');
 });
 
 app.get('/contact', function(req, res){
   res.send('This is contact page.');
 });
 
-app.get('/profile/:id', function(req, res){
-  res.send('Your ID is - ' + req.params.id);
+app.get('/profile/:name', function(req, res){
+  var data = {age: 27, job: 'Web Developer', hobbies: ['Eating', 'Gaming', 'Music']};
+  res.render('profile', {name: req.params.name, data: data});
 });
 
 app.listen(3000);
